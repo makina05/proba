3ae0f6a04a2a2d0367a5e013ea676d9b108650ab
jonblasco07 <jon.blasco@elcarmelo.net>
2020-09-23 16:02:26 +0200

    jaon naiz gehitu da funtzionalitate berrietan

--- src/main/java/com/isad/bertsioak/Funtzionalitate.java
+++ src/main/java/com/isad/bertsioak/Funtzionalitate.java
@@ -3,5 +3,6 @@ package com.isad.bertsioak;
 public class Funtzionalitate {
 	public static void main(String args[]){
 		System.out.println("Hemen kode konplexu bat izan beharko genuke :-) ");
+		System.out.println("Jon Naiz");
 	}
 }
